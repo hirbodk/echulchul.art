ed39c52754a1e4cf24cba65dee67fb948db11b58
Hirbod Kamalinia <hirbod@Mac.fritz.box>
2026-03-02 00:42:40 +0100

    update passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,21 +1,30 @@
 import sys
 import os
+import traceback
 
-# Explicitly add venv packages to path FIRST before anything else
-venv_path = '/home/echumggg/echulchul.art/venv/lib64/python3.12/site-packages'
-venv_path2 = '/home/echumggg/echulchul.art/venv/lib/python3.12/site-packages'
-sys.path.insert(0, venv_path)
-sys.path.insert(0, venv_path2)
-sys.path.insert(0, '/home/echumggg/echulchul.art')
+try:
+    venv_path = '/home/echumggg/echulchul.art/venv/lib64/python3.12/site-packages'
+    venv_path2 = '/home/echumggg/echulchul.art/venv/lib/python3.12/site-packages'
+    sys.path.insert(0, venv_path)
+    sys.path.insert(0, venv_path2)
+    sys.path.insert(0, '/home/echumggg/echulchul.art')
 
-import pymysql
-pymysql.version_info = (2, 2, 1, "final", 0)
-pymysql.install_as_MySQLdb()
+    import pymysql
+    pymysql.version_info = (2, 2, 1, "final", 0)
+    pymysql.install_as_MySQLdb()
 
-from dotenv import load_dotenv
-load_dotenv('/home/echumggg/echulchul.art/.env')
+    from dotenv import load_dotenv
+    load_dotenv('/home/echumggg/echulchul.art/.env')
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'echulchul.settings.production'
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'echulchul.settings.production'
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
+
+except Exception as e:
+    error_msg = traceback.format_exc()
+    with open('/home/echumggg/echulchul.art/wsgi_error.log', 'w') as f:
+        f.write(error_msg)
+    def application(environ, start_response):
+        start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
+        return [error_msg.encode()]
