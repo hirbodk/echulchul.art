d2fb68e9736deb4b0402f89e6746f0a89817f91e
Hirbod Kamalinia <hirbod@Mac.fritz.box>
2026-03-02 00:40:19 +0100

    add passenger trace

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,16 +1,34 @@
-import os
 import sys
-from dotenv import load_dotenv
+import os
 
-load_dotenv('/home/echumggg/echulchul.art/.env')
+# Log errors to a file we can read
+import traceback
 
-sys.path.insert(0, '/home/echumggg/echulchul.art')
+try:
+    import pymysql
+    pymysql.version_info = (2, 2, 1, "final", 0)
+    pymysql.install_as_MySQLdb()
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'echulchul.settings.production'
+    from dotenv import load_dotenv
+    load_dotenv('/home/echumggg/echulchul.art/.env')
 
-activate_env = '/home/echumggg/echulchul.art/venv/bin/activate_this.py'
-with open(activate_env) as f:
-    exec(f.read(), {'__file__': activate_env})
+    sys.path.insert(0, '/home/echumggg/echulchul.art')
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'echulchul.settings.production'
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+    activate_env = '/home/echumggg/echulchul.art/venv/bin/activate_this.py'
+    with open(activate_env) as f:
+        exec(f.read(), {'__file__': activate_env})
+
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
+
+except Exception as e:
+    error_msg = traceback.format_exc()
+    # Write error to a readable file
+    with open('/home/echumggg/echulchul.art/wsgi_error.log', 'w') as f:
+        f.write(error_msg)
+
+    # Also show it in the browser temporarily
+    def application(environ, start_response):
+        start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
+        return [error_msg.encode()]
