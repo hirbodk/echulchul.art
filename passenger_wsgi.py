cd4cb0f9ca31eceb745cc320212aae551fd854e3
Hirbod Kamalinia <hirbod@Mac.fritz.box>
2026-03-02 00:26:45 +0100

    update wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,17 +2,13 @@ import os
 import sys
 from dotenv import load_dotenv
 
-# Load environment variables
-load_dotenv('/home/echumggg/echulchul_app/.env')
+load_dotenv('/home/echumggg/echulchul.art/.env')
 
-# Add project to path
-sys.path.insert(0, '/home/echumggg/echulchul_app')
+sys.path.insert(0, '/home/echumggg/echulchul.art')
 
-# Point to production settings
 os.environ['DJANGO_SETTINGS_MODULE'] = 'echulchul.settings.production'
 
-# Activate virtualenv
-activate_env = '/home/echumggg/echulchul_app/venv/bin/activate_this.py'
+activate_env = '/home/echumggg/echulchul.art/venv/bin/activate_this.py'
 with open(activate_env) as f:
     exec(f.read(), {'__file__': activate_env})
 
