07995060b61aaff030e52f1d3108e13bb26b6df5
Hirbod Kamalinia <hirbod@Mac.fritz.box>
2026-03-02 01:48:39 +0100

    update home

--- home/models.py
+++ home/models.py
@@ -1,7 +1,20 @@
 from django.db import models
-
 from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    tagline = models.CharField(
+        max_length=255,
+        blank=True,
+        default='Art Collective'
+    )
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('tagline'),
+        FieldPanel('intro'),
+    ]
+
+    max_count = 1
