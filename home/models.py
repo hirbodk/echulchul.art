13adb7af5071ce4b79f65559c5d4d90d27d55340
Hirbod Kamalinia <hirbod@Mac.fritz.box>
2026-03-02 01:49:34 +0100

    update home

--- home/models.py
+++ home/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel
+from modelcluster.fields import ParentalKey
+from wagtail.models import Orderable
+
+
+class HomePageLink(Orderable):
+    page = ParentalKey('home.HomePage', on_delete=models.CASCADE, related_name='links')
+    label = models.CharField(max_length=100)
+    url = models.CharField(max_length=255)
+
+    panels = [
+        FieldPanel('label'),
+        FieldPanel('url'),
+    ]
 
 
 class HomePage(Page):
@@ -15,6 +28,9 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('tagline'),
         FieldPanel('intro'),
+        MultiFieldPanel([
+            InlinePanel('links', label='Link'),
+        ], heading='Navigation Links'),
     ]
 
     max_count = 1
